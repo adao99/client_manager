58bbfb8f87d839d42344026730e3260e64a956dc
Diogo Adão <dev_diogo.adao@hotmail.com>
2023-08-28 17:31:41 +0100

    added better matcher for middleware

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -17,5 +17,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/:path*",
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
